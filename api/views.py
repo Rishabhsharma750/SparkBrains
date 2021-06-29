3d86f9a7df49f8c020ed5872eb6947ce69b4a2a0
Lenovo <rishabhsharma750@gmail.com>
2021-06-29 18:37:09 +0530

    did CRUD using ModelViewSet

--- api/views.py
+++ api/views.py
@@ -20,35 +20,43 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields=('name','email',)
 
-class UserViewSet(viewsets.ViewSet):
-    def list(self, request):
-        queryset = Student.objects.all()
-        serializer = StudentSerializer(queryset, many=True)
-        return Response(serializer.data)
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    # authentication_classes = (TokenAuthentication,)
+    # permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'city',)
 
-    def create(self, request):
-        serializer = StudentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        stu = Student.objects.get(id=pk)
-        serializer = StudentSerializer(stu,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        queryset = Student.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = StudentSerializer(user)
-        return Response(serializer.data)
-
-    def destroy(self,request,pk):
-        stu=Student.objects.get(pk=id)
-        stu.delete()
-        return Response({'msg':'Data deleted'})
+# class UserViewSet(viewsets.ViewSet):
+#     def list(self, request):
+#         queryset = Student.objects.all()
+#         serializer = StudentSerializer(queryset, many=True)
+#         return Response(serializer.data)
+#
+#     def create(self, request):
+#         serializer = StudentSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#         return Response(serializer.data)
+#
+#     def update(self, request, pk=None):
+#         stu = Student.objects.get(id=pk)
+#         serializer = StudentSerializer(stu,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#         return Response(serializer.data)
+#
+#     def retrieve(self, request, pk=None):
+#         queryset = Student.objects.all()
+#         user = get_object_or_404(queryset, pk=pk)
+#         serializer = StudentSerializer(user)
+#         return Response(serializer.data)
+#
+#     def destroy(self,request,pk):
+#         stu=Student.objects.get(pk=id)
+#         stu.delete()
+#         return Response({'msg':'Data deleted'})
 
 class UserLoginView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
